53d958e987efcb7d91c4825c5c67ae8e190eb66a
ikechukwu-peter <pitiyke@gmail.com>
2022-03-29 16:57:10 +0100

    feat: token clearing on expiration

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import jwtDecode from 'jwt-decode'
+import { useRouter } from 'next/router'
 
+let token = jwtDecode(localStorage.getItem('token'))
+//Check for expired token
+const currentTime = Date.now() / 1000;
+if (token.exp < currentTime) {
+  localStorage.clear()
+  router.push('/login')
+}
 
 function MyApp({ Component, pageProps }) {
 
