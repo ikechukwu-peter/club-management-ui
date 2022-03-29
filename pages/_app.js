7c858c8c071dd4e1216f53b6ab21e8df587921cb
Ikechukwu Peter <60905186+ikechukwu-peter@users.noreply.github.com>
2022-03-29 20:17:43 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,21 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import jwtDecode from 'jwt-decode'
 
+
+
+function MyApp({ Component, pageProps }) {
 if (typeof window !== "undefined") {
 let token = localStorage.getItem('token')
   if (token) {
      let decoded = jwtDecode(token)
-   //Check for expired token   const currentTime = Date.now() / 1000;
 
+   //Check for expired token   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     localStorage.clear()
     window.location.href = '/login'
    }
   }
- }
-function MyApp({ Component, pageProps }) {
-
+} 
   return (
     <ChakraProvider resetCSS>
       <Header />
